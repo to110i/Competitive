9c4053422453709fa907620d7197567f60123055
to110i <beealive1213@gmail.com>
2021-02-09 21:44:57 +0900

    commit from lab

--- AOJ/DPL/dpl01b.cpp
+++ AOJ/DPL/dpl01b.cpp
@@ -10,4 +10,24 @@
 using namespace std;
 typedef long long int lli;
 
-int main() { return 0; }
+int main() {
+    int N, W;
+    int v[200], w[200];
+    int dp[200][10200];
+    cin >> N >> W;
+    for (int i = 0; i < N; i++) {
+        cin >> v[i] >> w[i];
+    }
+    for (int i = 0; i < W; i++) dp[0][i] = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j <= W; j++) {
+            if (w[i] <= j) {
+                dp[i + 1][j] = max(dp[i][j - w[i]] + v[i], dp[i][j]);
+            } else {
+                dp[i + 1][j] = dp[i][j];
+            }
+        }
+    }
+    cout << dp[N][W] << endl;
+    return 0;
+}
---
+++ AOJ/DPL/dpl01c.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+typedef long long int lli;
+
+int main() {
+    int N, W;
+    int v[200], w[200];
+    int dp[200][10200];
+
+    cout << dp[N][W] << endl;
+    return 0;
+}
