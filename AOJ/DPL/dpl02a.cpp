57795594f4c8b187879f9ae5f8b6747931f9d9e4
to110i <beealive1213@gmail.com>
2021-02-15 15:06:18 +0900

    add from lab

---
+++ AOJ/DPL/dpl02a.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+typedef long long int lli;
+
+int main() {
+    int V, E;
+    cin >> V >> E;
+
+    return 0;
+}
---
+++ AOJ/Volume16/1611.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+typedef long long int lli;
+
+int main() {
+    int N;
+    cin >> N;
+    while (N != 0) {
+        int w[500];
+        for (int i = 0; i < N; i++) {
+            cin >> w[i];
+        }
+    }
+    return 0;
+}
